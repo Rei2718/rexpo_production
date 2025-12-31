6825f2a0d9db4a2fa2217af81715bcd4eadc3e98
Rei2718 <once2718@gmail.com>
2025-12-31 14:14:05 +0900

    feat: add storage buckets and configure public read access policies for various entities.

--- supabase/snippets/Untitled query 527.sql
+++ supabase/snippets/Untitled query 527.sql
@@ -1,18 +1,4 @@
--- storage buckets
-INSERT INTO storage.buckets (id, name, public)
-VALUES 
-    ('categories',    'categories',    true),
-    ('features',      'features',      true),
-    ('venues',        'venues',        true),
-    ('organizations', 'organizations', true),
-    ('foods',         'foods',         true),
-    ('performers',    'performers',    true),
-    ('events',        'events',        true),
-    ('news',          'news',          true),
-    ('banners',       'banners',       true)
-ON CONFLICT (id) DO NOTHING;
-
--- storage policies
+-- fix storage policies
 DROP POLICY IF EXISTS "Public Access Multi" ON storage.objects;
 
 -- categories
