958a740cfcaf4ebe63d68cb76a5ea93698eb9e5e
Rei2718 <once2718@gmail.com>
2026-01-02 05:49:50 +0900

    feat: Add tabs index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -25,14 +25,6 @@ export default function IndexScreen() {
             description="現在表示されているデータは全てダミーデータです。"
           />
           <ProgressItem
-            title="ブックマーク機能"
-            description="ブックマーク機能自体は実装されていますが、ブックマーク済みコンテンツを確認する画面は未実装です。"
-          />
-          <ProgressItem
-            title="モーダル"
-            description="CategoryおよびTagのモーダルスクリーンは実装されていません。"
-          />
-          <ProgressItem
             title="マップ機能"
             description={`カスタムマップオーバーレイは未実装です。\nマーカー処理にバグがあります。\nボトムシートの開閉動作にバグがあります。`}
           />
