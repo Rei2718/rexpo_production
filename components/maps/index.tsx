e0b58b0c713c380c7df2cd0e873c02364dfdc4f6
Rei2718 <once2718@gmail.com>
2026-01-10 02:50:46 +0900

    feat: map bottom sheet

--- components/maps/index.tsx
+++ components/maps/index.tsx
@@ -1,17 +1,28 @@
+import Sheet from "@/components/map/sheet";
 import { StatusMessage } from "@/components/ui/status-message";
 import { useAllVenues } from "@/supabase/api";
+import { DisplayVenue, Verified } from "@/supabase/api/types";
+import { useState } from "react";
 import { View } from "react-native";
 import MapsView from "./maps-view";
 
 export default function MapsScreen() {
     const { data: venues, isPending, isError } = useAllVenues();
+    const [selectedVenue, setSelectedVenue] = useState<Verified<DisplayVenue> | null>(null);
 
     if (isPending) return <StatusMessage status="loading" />;
     if (isError) return <StatusMessage status="error" />;
 
     return (
         <View style={{ flex: 1 }}>
-            <MapsView venues={venues ?? []} />
+            <MapsView
+                venues={venues ?? []}
+                onMarkerPress={setSelectedVenue}
+            />
+            <Sheet
+                data={selectedVenue}
+                onClose={() => setSelectedVenue(null)}
+            />
         </View>
     );
 }
