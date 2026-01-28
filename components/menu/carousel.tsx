ea1ef2b00475facf153faf5117a9169bb3077df4
Rei2718 <once2718@gmail.com>
2026-01-28 15:59:23 +0900

    feat: add announcement carousel to the menu screen.

---
+++ components/menu/carousel.tsx
@@ -0,0 +1,73 @@
+
+import { ThemedView } from "@/components/ui/themed-view";
+import { Spacing } from "@/constants/theme";
+import { useThemeColor } from "@/hooks/use-theme-color";
+import { Image } from "expo-image";
+import { StyleSheet, useWindowDimensions } from "react-native";
+import Carousel from "react-native-reanimated-carousel";
+
+const carouselImages = [
+    require("@/assets/event-carousel/1.png"),
+    require("@/assets/event-carousel/2.png"),
+    require("@/assets/event-carousel/3.png"),
+    require("@/assets/event-carousel/4.png"),
+    require("@/assets/event-carousel/5.png"),
+];
+
+export function MenuCarousel() {
+    const color = useThemeColor();
+    const { width } = useWindowDimensions();
+
+    const carouselWidth = width;
+    const carouselHeight = (width - Spacing.s20 * 2) / 3;
+
+    return (
+        <ThemedView>
+            <Carousel
+                loop={true}
+                width={carouselWidth}
+                height={carouselHeight}
+                snapEnabled={true}
+                pagingEnabled={true}
+                data={carouselImages}
+                style={styles.carousel}
+                renderItem={({ item }) => (
+                    <ThemedView style={styles.itemContainer}>
+                        <Image
+                            source={item}
+                            contentFit="cover"
+                            style={[
+                                styles.imageBackground,
+                                {
+                                    borderRadius: Spacing.s20,
+                                    borderWidth: Spacing.s1,
+                                    borderColor: color.border,
+                                    backgroundColor: color.natural_500,
+                                }
+                            ]}
+                        />
+                    </ThemedView>
+                )}
+                onConfigurePanGesture={gesture => {
+                    'worklet';
+                    gesture.activeOffsetX([-10, 10]);
+                    gesture.failOffsetY([-5, 5]);
+                }}
+            />
+        </ThemedView>
+    );
+}
+
+const styles = StyleSheet.create({
+    carousel: {
+        width: "100%",
+    },
+    itemContainer: {
+        flex: 1,
+        paddingHorizontal: Spacing.s20,
+    },
+    imageBackground: {
+        width: "100%",
+        height: "100%",
+    },
+});
--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -1,3 +1,4 @@
+import { MenuCarousel } from "@/components/menu/carousel";
 import { SettingsLinkItem } from "@/components/settings/settings-link-item";
 import { SettingsSection } from "@/components/settings/settings-section";
 import { Container } from "@/components/ui/container";
@@ -34,86 +35,97 @@ export default function MenuScreen() {
                 }}
             />
             <ScrollView contentInsetAdjustmentBehavior="automatic" showsVerticalScrollIndicator={false}>
-                <Container paddingHorizontal="s20" gap="s32" style={{ paddingBottom: modal }}>
-                    {/* Features */}
-                    <SettingsSection title="その他">
-                        <SettingsLinkItem
-                            label="フード"
-                            icon="food"
-                            href="/(detail)/food"
-                        />
-                        <SettingsLinkItem
-                            label="アンケート"
-                            icon="survey"
-                            href="/(detail)/survey"
-                        />
-                    </SettingsSection>
-
-                    {/* App Info */}
-                    <SettingsSection title="アプリについて">
-                        <SettingsLinkItem
-                            label="利用規約"
-                            icon="contract"
-                            href="/(detail)/terms"
-                        />
-                        <SettingsLinkItem
-                            label="プライバシーポリシー"
-                            icon="contract"
-                            href="/(detail)/privacy"
-                        />
-                        <SettingsLinkItem
-                            label="ライセンス"
-                            icon="verified"
-                            href="/(detail)/licenses"
-                        />
-                        <SettingsLinkItem
-                            label="クレジット"
-                            icon="humanFill"
-                            href="/(detail)/credits"
-                        />
-                        <SettingsLinkItem
-                            label="コア・コンセプト"
-                            icon="pentagon"
-                            href="/(detail)/core-concept"
-                            isLast={true}
-                        />
-                    </SettingsSection>
-
-
-                    {/* Support */}
-                    <SettingsSection title="サポート">
-                        <SettingsLinkItem
-                            label="バグ報告"
-                            icon="link"
-                            href="https://docs.google.com/forms/d/e/1FAIpQLSdPEU1ETnndLiPo4Wy1vszlxvkzTG9uObHesd-ZSlDMyYH8Cw/viewform?usp=dialog"
-                        />
-                        <SettingsLinkItem
-                            label="立命館慶祥中学・高等学校"
-                            icon="link"
-                            href="https://www2.spc.ritsumei.ac.jp"
-                            isLast={true}
-                        />
-                    </SettingsSection>
-
-                    {/* Settings */}
-                    <SettingsSection title="アプリ設定">
-                        <SettingsLinkItem
-                            label="設定"
-                            icon="gear"
-                            href="/(detail)/settings"
-                            isLast={true}
-                        />
-                    </SettingsSection>
-
-                    {/* Version Footer */}
-
-
-                    <Container alignItems="center">
-                        <ThemedText type="caption1">
-                            v5.6.3
-                        </ThemedText>
+                <Container gap="s32" style={{ paddingBottom: modal }}>
+                    <Container gap="s8" paddingTop="s20">
+                        <Container paddingHorizontal="s36">
+                            <ThemedText type="footnote" color="natural_200">
+                                お知らせ
+                            </ThemedText>
+                        </Container>
+                        <MenuCarousel />
                     </Container>
 
+                    <Container paddingHorizontal="s20" gap="s32">
+                        {/* Features */}
+                        <SettingsSection title="その他">
+                            <SettingsLinkItem
+                                label="フード"
+                                icon="food"
+                                href="/(detail)/food"
+                            />
+                            <SettingsLinkItem
+                                label="アンケート"
+                                icon="survey"
+                                href="/(detail)/survey"
+                            />
+                        </SettingsSection>
+
+                        {/* App Info */}
+                        <SettingsSection title="アプリについて">
+                            <SettingsLinkItem
+                                label="利用規約"
+                                icon="contract"
+                                href="/(detail)/terms"
+                            />
+                            <SettingsLinkItem
+                                label="プライバシーポリシー"
+                                icon="contract"
+                                href="/(detail)/privacy"
+                            />
+                            <SettingsLinkItem
+                                label="ライセンス"
+                                icon="verified"
+                                href="/(detail)/licenses"
+                            />
+                            <SettingsLinkItem
+                                label="クレジット"
+                                icon="humanFill"
+                                href="/(detail)/credits"
+                            />
+                            <SettingsLinkItem
+                                label="コア・コンセプト"
+                                icon="pentagon"
+                                href="/(detail)/core-concept"
+                                isLast={true}
+                            />
+                        </SettingsSection>
+
+
+                        {/* Support */}
+                        <SettingsSection title="サポート">
+                            <SettingsLinkItem
+                                label="バグ報告"
+                                icon="link"
+                                href="https://docs.google.com/forms/d/e/1FAIpQLSdPEU1ETnndLiPo4Wy1vszlxvkzTG9uObHesd-ZSlDMyYH8Cw/viewform?usp=dialog"
+                            />
+                            <SettingsLinkItem
+                                label="立命館慶祥中学・高等学校"
+                                icon="link"
+                                href="https://www2.spc.ritsumei.ac.jp"
+                                isLast={true}
+                            />
+                        </SettingsSection>
+
+                        {/* Settings */}
+                        <SettingsSection title="アプリ設定">
+                            <SettingsLinkItem
+                                label="設定"
+                                icon="gear"
+                                href="/(detail)/settings"
+                                isLast={true}
+                            />
+                        </SettingsSection>
+
+                        {/* Version Footer */}
+
+
+                        <Container alignItems="center">
+                            <ThemedText type="caption1">
+                                v5.6.3
+                            </ThemedText>
+                        </Container>
+
+                    </Container >
                 </Container >
             </ScrollView >
         </>
