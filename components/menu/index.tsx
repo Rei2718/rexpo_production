3d1debaf3a3c1646e9b6b5bffa03a45cd4bc963c
Rei2718 <once2718@gmail.com>
2026-02-02 14:28:27 +0900

    chore: update displayed version number to v5.6.6

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -121,7 +121,7 @@ export default function MenuScreen() {
 
                         <Container alignItems="center">
                             <ThemedText type="caption1">
-                                v5.6.5 - dev
+                                v5.6.6 - dev
                             </ThemedText>
                         </Container>
 
