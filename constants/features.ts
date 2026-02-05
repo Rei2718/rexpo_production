b5d34694e940a0fdee8bfcf895a940372b389539
Rei2718 <once2718@gmail.com>
2026-02-05 18:33:41 +0900

    chore: update feature flag definitions.

--- constants/features.ts
+++ constants/features.ts
@@ -20,15 +20,6 @@ export const STATIC_FEATURES: Verified<Feature>[] = [
         display_order: 2,
     },
     {
-        feature_public_id: "artist",
-        name: "HIPPY",
-        caption: "1億人の背中を押した令和の応援ソング\n『君に捧げる応援歌』",
-        note: "MVを見る",
-        image: require('@/assets/features/artist.jpg') as any,
-        event_public_id: "https://youtu.be/DiRXDpDL7es?si=hSFcNJSEHPfdMF8d",
-        display_order: 3,
-    },
-    {
         feature_public_id: "eco",
         name: "エコステーション",
         caption: "ゴミはエコステーションまでお持ちください",
@@ -38,3 +29,15 @@ export const STATIC_FEATURES: Verified<Feature>[] = [
         display_order: 3,
     }
 ];
+
+{/*
+    {
+        feature_public_id: "artist",
+        name: "HIPPY",
+        caption: "1億人の背中を押した令和の応援ソング\n『君に捧げる応援歌』",
+        note: "MVを見る",
+        image: require('@/assets/features/artist.jpg') as any,
+        event_public_id: "https://youtu.be/DiRXDpDL7es?si=hSFcNJSEHPfdMF8d",
+        display_order: 3,
+    },
+*/}
